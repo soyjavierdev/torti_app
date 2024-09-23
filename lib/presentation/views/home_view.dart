2216ec4341c4b566b8214658f3c7289612aef0b4
Javier Ruiz <javierdeveloper1@gmail.com>
2024-09-23 12:34:14 +0200

    added buttons increase omelette

--- lib/presentation/views/home_view.dart
+++ lib/presentation/views/home_view.dart
@@ -2,6 +2,7 @@ import 'dart:convert';
 
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:torti_app/presentation/widgets/home_groups.dart';
 import 'package:torti_app/presentation/widgets/home_stat_widget.dart';
 import 'package:torti_app/presentation/widgets/sidebar_widget.dart';
 
@@ -49,10 +50,9 @@ class _HomeScreenState extends State<HomeScreen> {
     return '${lastUser['name']} ${lastUser['lastname']} ${lastUser['omelettePaid']}';
   }
 
-  // Función para sumar tortillas pagadas
+    //Función para incrementar omelette pagadas
   void incrementOmelettePaid(int group, int index, double amount) {
     setState(() {
-      // Filtra a los usuarios del grupo específico y suma
       users.where((user) => user['grupo'] == group).toList()[index]
           ['omelettePaid'] += amount;
     });
@@ -80,265 +80,30 @@ class _HomeScreenState extends State<HomeScreen> {
       body: Row(
         children: [
           //^ Lista del Grupo 0
-          ConstrainedBox(
-            constraints:
-                BoxConstraints(maxWidth: MediaQuery.sizeOf(context).width / 4),
-            child: Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  HomeStat(
-                    color: Colors.red,
-                    titleText: 'Más tortillas pagadas',
-                    subheading: ' ${getUserWithMoreOmelettes(users)}',
-                  ),
-                  const Text(
-                    'Grupo 0',
-                    style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-                  ),
-                  const SizedBox(height: 8),
-                  // Use ListView to allow scrolling
-                  Expanded(
-                    child: ListView.builder(
-                      itemCount: group0.length,
-                      itemBuilder: (context, index) {
-                        var e = group0[index];
-
-                        return Center(
-                          // Center the ListTile
-                          child: ListTile(
-                            trailing: Row(
-                              mainAxisSize: MainAxisSize
-                                  .min, // Adjust size to fit content
-                              children: [
-                                IconButton(
-                                  icon: const Icon(Icons.add),
-                                  onPressed: () {
-                                    incrementOmelettePaid(
-                                        0, index, 1.0); // Increment by 1
-                                  },
-                                  tooltip: 'Agregar 1 Tortilla',
-                                ),
-                                IconButton(
-                                  icon: const Icon(Icons.add_circle),
-                                  onPressed: () {
-                                    incrementOmelettePaid(
-                                        0, index, 0.5); // Increment by 0.5
-                                  },
-                                  tooltip: 'Agregar 0.5 Tortilla',
-                                ),
-                              ],
-                            ),
-                            title: Center(
-                                child: Text('${e['name']} ${e['lastname']}')),
-                            subtitle: Center(
-                                child: Text(
-                                    'Tortillas Pagadas: ${e['omelettePaid']}')),
-                          ),
-                        );
-                      },
-                    ),
-                  ),
-                ],
-              ),
-            ),
-          ),
-
+          HomeGroups(
+              users: group0,
+              colors: const Color(0xFFE9FFF3),
+              titleText: 'Más tortillas pagadas',
+              subheading: getUserWithMoreOmelettes(users),
+              incrementOmelettePaid: incrementOmelettePaid),
           //^ GRUPO 1
-          ConstrainedBox(
-            constraints:
-                BoxConstraints(maxWidth: MediaQuery.sizeOf(context).width / 4),
-            child: Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  HomeStat(
-                    color: Colors.blue,
-                    titleText: 'Te toca ir pagando...',
-                    subheading: ' ${getUserWithLessOmelettes(users)}',
-                  ),
-                  const Text(
-                    'Grupo 1',
-                    style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-                  ),
-                  const SizedBox(height: 8),
-                  // Use ListView to allow scrolling
-                  Expanded(
-                    child: ListView(
-                      children: group1.map((e) {
-                        return Center(
-                          // Center the ListTile
-                          child: ListTile(
-                            title: Center(
-                                child: Text('${e['name']} ${e['lastname']}')),
-                            subtitle: Center(
-                                child: Text(
-                                    'Tortillas Pagadas: ${e['omelettePaid']}')),
-                          ),
-                        );
-                      }).toList(),
-                    ),
-                  ),
-                ],
-              ),
-            ),
-          ),
-
+          HomeGroups(
+              users: group1,
+              colors:  const Color(0xFFFFE9E9),
+              titleText: 'Te toca ir pagando',
+              subheading: getUserWithLessOmelettes(users),
+              incrementOmelettePaid: incrementOmelettePaid),
           //^ GRUPO 2
-          ConstrainedBox(
-            constraints:
-                BoxConstraints(maxWidth: MediaQuery.sizeOf(context).width / 4),
-            child: Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  const HomeStat(
-                    color: Colors.yellow,
-                    titleText: 'Ruleta',
-                    subheading: 'ee',
-                  ),
-                  const Text(
-                    'Grupo 2',
-                    style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-                  ),
-                  const SizedBox(height: 8),
-                  // Use ListView to allow scrolling
-                  Expanded(
-                    child: ListView(
-                      children: group2.map((e) {
-                        return Center(
-                          // Center the ListTile
-                          child: ListTile(
-                            title: Center(
-                                child: Text('${e['name']} ${e['lastname']}')),
-                            subtitle: Center(
-                                child: Text(
-                                    'Tortillas Pagadas: ${e['omelettePaid']}')),
-                          ),
-                        );
-                      }).toList(),
-                    ),
-                  ),
-                ],
-              ),
-            ),
-          ),
-        
+          HomeGroups(
+              users: group2,
+              colors:  const Color(0xFFFFFCE9),
+              titleText: 'Ruleta',
+              subheading: 'Prueba suerte',
+              incrementOmelettePaid: incrementOmelettePaid),
+
           const SideBar(),
         ],
       ),
     );
   }
-
-  Widget buildGroupList(List<dynamic> group, Color color, String title, String subheading) {
-  return ConstrainedBox(
-    constraints: BoxConstraints(maxWidth: MediaQuery.sizeOf(context).width / 4),
-    child: Padding(
-      padding: const EdgeInsets.all(16.0),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          HomeStat(
-            color: color,
-            titleText: title,
-            subheading: subheading,
-          ),
-          const SizedBox(height: 8),
-          const Text(
-            'Grupo',
-            style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-          ),
-          const SizedBox(height: 8),
-          Expanded(
-            child: ListView.builder(
-              itemCount: group.length,
-              itemBuilder: (context, index) {
-                var e = group[index];
-
-                return Center(
-                  child: ListTile(
-                    trailing: Row(
-                      mainAxisSize: MainAxisSize.min,
-                      children: [
-                        IconButton(
-                          icon: const Icon(Icons.add),
-                          onPressed: () {
-                            incrementOmelettePaid(e['grupo'], index, 1.0);
-                          },
-                          tooltip: 'Agregar 1 Tortilla',
-                        ),
-                        IconButton(
-                          icon: const Icon(Icons.add_circle),
-                          onPressed: () {
-                            incrementOmelettePaid(e['grupo'], index, 0.5);
-                          },
-                          tooltip: 'Agregar 0.5 Tortilla',
-                        ),
-                      ],
-                    ),
-                    title: Center(child: Text('${e['name']} ${e['lastname']}')),
-                    subtitle: Center(child: Text('Tortillas Pagadas: ${e['omelettePaid']}')),
-                  ),
-                );
-              },
-            ),
-          ),
-        ],
-      ),
-    ),
-  );
-}
-
-@override
-Widget build(BuildContext context) {
-      final List<dynamic> group0 =
-        users.where((user) => user['grupo'] == 0).toList();
-    final List<dynamic> group1 =
-        users.where((user) => user['grupo'] == 1).toList();
-    final List<dynamic> group2 =
-        users.where((user) => user['grupo'] == 2).toList();
-  return Scaffold(
-    appBar: AppBar(
-      title: Image.asset(
-        'assets/images/logo-torti-app.png',
-        fit: BoxFit.contain,
-        height: 50,
-      ),
-      centerTitle: true,
-    ),
-    body: Row(
-      children: [
-        // Grupo 0
-        buildGroupList(
-          group0,
-          Colors.red,
-          'Más tortillas pagadas',
-          ' ${getUserWithMoreOmelettes(users)}',
-        ),
-
-        // Grupo 1
-        buildGroupList(
-          group1,
-          Colors.blue,
-          'Te toca ir pagando...',
-          ' ${getUserWithLessOmelettes(users)}',
-        ),
-
-        // Grupo 2
-        buildGroupList(
-          group2,
-          Colors.yellow,
-          'Ruleta',
-          'ee',
-        ),
-
-        const SideBar(),
-      ],
-    ),
-  );
-}
-
 }
---
+++ lib/presentation/widgets/home_groups.dart
@@ -0,0 +1,85 @@
+import 'package:flutter/material.dart';
+import 'package:torti_app/presentation/widgets/home_stat_widget.dart';
+
+class HomeGroups extends StatelessWidget {
+  const HomeGroups(
+      {super.key,
+      required this.users,
+      required this.colors,
+      required this.titleText,
+      required this.subheading,
+      required this.incrementOmelettePaid});
+
+  final List<dynamic> users;
+  final Color colors;
+  final String titleText;
+  final String subheading;
+  final Function incrementOmelettePaid;
+
+  @override
+  Widget build(BuildContext context) {
+    return ConstrainedBox(
+      constraints:
+          BoxConstraints(maxWidth: MediaQuery.sizeOf(context).width / 4),
+      child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            HomeStat(
+              color: colors,
+              titleText: titleText,
+              subheading: subheading,
+            ),
+            const Text(
+              'Grupo 0',
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(height: 8),
+         
+            Expanded(
+              child: ListView.builder(
+                itemCount: users.length,
+                itemBuilder: (context, index) {
+                  var e = users[index];
+                  return Center(
+                    // Center the ListTile
+                    child: ListTile(
+                      trailing: Row(
+                        mainAxisSize:
+                            MainAxisSize.min, // Adjust size to fit content
+                        children: [
+                          IconButton(
+                            icon: const Icon(Icons.add),
+                            onPressed: () {
+                              incrementOmelettePaid(
+                                  e['grupo'], index, 1); // Increment by 1
+                            },
+                            tooltip: 'Agregar 1 Tortilla',
+                          ),
+                          IconButton(
+                            icon: const Icon(Icons.add_circle),
+                            onPressed: () {
+                              incrementOmelettePaid(
+                                  e['grupo'], index, 0.5); // Increment by 0.5
+                            },
+                            tooltip: 'Agregar 0.5 Tortilla',
+                          ),
+                        ],
+                      ),
+                      title:
+                          Center(child: Text('${e['name']} ${e['lastname']}')),
+                      subtitle: Center(
+                          child:
+                              Text('Tortillas Pagadas: ${e['omelettePaid']}')),
+                    ),
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
